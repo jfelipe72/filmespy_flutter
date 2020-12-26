48bada5d85eb3369be50533d871bd6b485896367
jfelipe72 <49200056+jfelipe72@users.noreply.github.com>
2020-12-26 17:43:51 -0300

    Create constants.dart

---
+++ lib/app/shared/constants.dart
@@ -0,0 +1,6 @@
+const String DB_MOVIES = "db_movies";
+const String TB_FAV_MOVIES = "tb_fav_movies";
+const String CREATE_TB_FAV_MOVIES =
+    "CREATE TABLE $TB_FAV_MOVIES (link TEXT PRIMARY KEY, titulo TEXT, data TEXT, genero TEXT, poster TEXT, sinopse TEXT, sinopseFull TEXT, isFavorite TEXT)";
+
+const String IMG_BACKGROUND = "assets/images/background.png";
